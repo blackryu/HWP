d2e5d9005b957040066878967eaea062607780ae
Philip <Philip@Philip-PC.fritz.box>
2014-10-30 12:09:41 +0100

    test js

---
+++ src/tinyVM/Main.java
@@ -0,0 +1,14 @@
+package tinyVM;
+
+public class Main {
+
+	public static void main(String[] args) {
+		VM vm = new VM();
+                   //git
+		vm.readFile("C:\\Users\\Philip\\workspace\\HWP\\src\\tinyVM\\text.txt");
+
+		// for(int i = 0; i < 4096; i++)
+		// System.out.println("Stelle " + i +": " + vm.getMemory(i));
+	}
+
+}
