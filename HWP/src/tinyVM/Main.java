59a1eb70baca3a3f431b452e0807ac7fd4191bff
Philip <Philip@141.19.179.38>
2014-10-31 13:24:40 +0100

    execute

--- HWP/src/tinyVM/Main.java
+++ HWP/src/tinyVM/Main.java
@@ -5,10 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		VM vm = new VM();
 
-		vm.readFile("C:\\Users\\Philip\\workspace\\HWP\\src\\tinyVM\\text.txt");
+		vm.readFile("../HWP/src/asm.txt");
+		vm.runVM();
 
-		// for(int i = 0; i < 4096; i++)
-		// System.out.println("Stelle " + i +": " + vm.getMemory(i));
+		 for(int i = 0; i < 5; i++)
+		 System.out.println("Stelle " + i +": " + vm.getMemory(i));
 	}
 
 }
--- HWP/src/tinyVM/VM.java
+++ HWP/src/tinyVM/VM.java
@@ -12,30 +12,30 @@ public class VM {
 
 	private int programCounter = 0;
 
-	private short memory[] = new short[4096];
+	private int memory[] = new int[4096];
 
-	// public short getMemory(int i){
-	// return memory[i];
-	// }
+	public int getMemory(int i) {
+		return memory[i];
+	}
 
 	Stack<Integer> registerStack = new Stack<Integer>();
 	Stack<Integer> subroutineStack = new Stack<Integer>();
 
-	public void readFile(String datei) {
+	public void readFile(String file) {
 		// initialize FileReader and BufferedReader
-		FileReader fr = null;
+		FileReader fileReader = null;
 		try {
-			fr = new FileReader(datei);
+			fileReader = new FileReader(file);
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		BufferedReader br = new BufferedReader(fr);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
 
 		// read file and call returnOpCode
 		String line = "";
 		try {
-			while ((line = br.readLine()) != null) {
+			while ((line = bufferedReader.readLine()) != null) {
 				memory[programCounter] = returnOpCode(line);
 				programCounter++;
 			}
@@ -45,33 +45,36 @@ public class VM {
 
 		// close FileReader and BufferedReader
 		try {
-			br.close();
+			bufferedReader.close();
 		} catch (IOException e2) {
 			e2.printStackTrace();
 		}
 
 		try {
-			fr.close();
+			fileReader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	public short returnOpCode(String command) {
-		String[] firstPart = new String[0];
-		firstPart = command.split(" ");
-		short opCode = 0;
+	public int returnOpCode(String command) {
+		String[] commandSplitted = new String[0];
+		commandSplitted = command.split(" ");
+		int opCode = 0;
 
-		switch (firstPart[0]) {
+		switch (commandSplitted[0]) {
 		case "NOP": {
 			return 0;
 		}
 		case "LOAD": {
 			System.out.println("LOAD gefunden!");
-			// LOAD 99999999 ?
-			opCode = 0001;
-			return opCode;
+			int value = Integer.parseInt(commandSplitted[1]);
+			// Shift for R0 and LOAD
+			value = value << 5;
+			// LOAD Bit
+			value += 1;
+			return value;
 		}
 		case "MOV": {
 			System.out.println("MOV gefunden!");
@@ -126,4 +129,28 @@ public class VM {
 		return opCode;
 	}
 
+	public void executeOpCode(int[] filledMemory) {
+		// go to first memory entry
+		programCounter = 0;
+		// 
+		for (programCounter = 0; programCounter < 4095; programCounter++) {
+			int command = filledMemory[programCounter];
+			switch (command & 15) {
+			case 0: {
+				break;
+			}
+			// LOAD
+			case 1: {
+				System.out.println("LOAD");
+				break;
+			}
+
+			}
+		}
+
+	}
+
+	public void runVM() {
+		executeOpCode(memory);
+	}
 }
