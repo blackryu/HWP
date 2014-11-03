7ffa48477dea1a67a53744dcc99bfea9d50e441e
quiN68 <philip.quintus@gmail.com>
2014-11-03 10:26:49 +0100

    asm.txt

--- HWP/src/tinyVM/Main.java
+++ HWP/src/tinyVM/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		VM vm = new VM();
 
-		vm.readFile("C:\\Users\\Philip\\workspace\\HWP\\src\\tinyVM\\text.txt");
+		vm.readFile("../HWP/src/asm.txt");
 
 		// for(int i = 0; i < 4096; i++)
 		// System.out.println("Stelle " + i +": " + vm.getMemory(i));
