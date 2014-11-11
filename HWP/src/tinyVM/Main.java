6985d597f9ec7a33a113a119246af67625350343
c.hamilton <cody.hamilton@stud.hs-mannheim.de>
2014-11-11 11:33:20 +0100

    not workling asm things
    Signed-off-by: c.hamilton <cody.hamilton@stud.hs-mannheim.de>

--- HWP/src/tinyVM/Main.java
+++ HWP/src/tinyVM/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		VM vm = new VM();
-		vm.runVM("../HWP/src/asm.txt"); 
+		vm.runVM("../HWP/src/newFibAsm.txt"); 
 		
-		for (int i = 1; i < 1022; i++)
+		for (int i = 1000; i < 1022; i++)
 			System.out.println("Stelle " + i + ": " + vm.getMemory(i));
 
 	}
